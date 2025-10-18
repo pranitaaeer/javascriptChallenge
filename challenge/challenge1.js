f5b43810b223d470b156c3427d1320c46cf78cc7
Pranita Aeer <aeerpranita@gmail.com>
2025-10-18 10:19:59 +0000

    complete challenge 1

---
+++ challenge/challenge1.js
@@ -0,0 +1,10 @@
+const userName="pranitaa"
+let fullName="Pranitaa Aeer"
+var lastName="Aeer"
+
+// userName="Ankitaa"
+fullName="Ankitaa Aeer"
+lastName="aeer"
+
+//console.log(userName) // const variable cannot reassign
+console.table({ fullName, lastName });
