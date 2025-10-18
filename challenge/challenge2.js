f1eff755fdce803d31a046bf4d043220f8590bb0
Pranita Aeer <aeerpranita@gmail.com>
2025-10-18 10:27:45 +0000

    challenge2 is complete

---
+++ challenge/challenge2.js
@@ -0,0 +1,9 @@
+let userName="Pranitaa" //string
+let age=20 //number
+let isPresent=true //boolean
+let college; //undefined
+let marks=null //object
+
+console.log(typeof marks);
+console.log(typeof isPresent);
+console.log(typeof college);
