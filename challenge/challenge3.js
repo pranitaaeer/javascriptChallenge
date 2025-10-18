0d06b2ce7573c3a13ef9cbd6ce1590f064d97aaf
Pranita Aeer <aeerpranita@gmail.com>
2025-10-18 10:34:16 +0000

    challenge3 is complete

---
+++ challenge/challenge3.js
@@ -0,0 +1,11 @@
+let num="45"
+let convertToNumber=Number(num)
+console.log("convert string into number",convertToNumber);
+
+let istTrue=true
+convertToNumber=Number(istTrue)
+console.log("convert boolean true into number",convertToNumber);
+
+istTrue=false
+convertToNumber=Number(istTrue)
+console.log("convert boolean false into number",convertToNumber);
