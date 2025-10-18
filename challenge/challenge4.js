db1ade23a34ce8e3d1d70df12bd6f3a79dc77115
Pranita Aeer <aeerpranita@gmail.com>
2025-10-18 11:31:12 +0000

    challenge4 is complete

---
+++ challenge/challenge4.js
@@ -0,0 +1,20 @@
+//operations
+
+console.log("2"+5);
+console.log(2+"5");
+console.log("2"+5+2);
+console.log(2+5+"2");  //left to right that's why first perform addition of number than concat 
+
+console.log(+true) //avoid it
+//prefix and postfix
+let x=5
+let y=x++
+console.log(`value of x is:${x} and y is:${y}`) //first value ko use kro baad mein increment kro
+
+let a=12
+let b=++a
+console.log(`value of a is:${a} and y is:${b}`) //first value ko ncrement kro baad mein use kro
+
+
+
+
