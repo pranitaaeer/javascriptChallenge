a4090240e1aae0910b0e153420a7e988ac65c19f
Pranita Aeer <aeerpranita@gmail.com>
2025-10-21 04:56:15 +0000

    update in challenge6

--- challenge/challenge6.js
+++ challenge/challenge6.js
@@ -4,32 +4,31 @@ let name="Pranita" //string
 let isTrue=true //boolean
 let age; //undefined
 let college=null //null
+let number=1245n
 let sym=Symbol('id')
-console.log(typeof college);
-console.log(typeof sym);
+console.log( "typeof null:",typeof college);
+console.log("typeof symbol:",typeof sym);
+console.log("typeof bigint:",typeof number);
 
-let name2=name
-name2="Ankita"
-console.log(name);
-console.log(name2); //call by value
 
 //Reference(Non-primitive )data type
 let arr=[1,2,3,4]
+console.log("typeof array:",typeof arr);
+
 let fun=()=>{
     console.log("Radhe Radhe");
     
 }
-console.log(typeof fun);
+console.log("typeof function:",typeof fun);
 
 let obj={
     name:"Pranita",
     class:'TY BCA',
     age:20
 }
-let obj2=obj
-obj2.age=21
-console.log(obj);
-console.log(obj2); //call by reference
+console.log("typeof object:",typeof obj);
+
+
 
 
 
