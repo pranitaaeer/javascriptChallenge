760bfcddaf69d770154df11e347f599d7d201fe6
Pranita Aeer <aeerpranita@gmail.com>
2025-10-21 05:43:29 +0000

    challenge9 is done

---
+++ challenge/challenge9.js
@@ -0,0 +1,18 @@
+let marks=75
+let num2=new Number(85.505)
+console.log(num2.toFixed(2));
+console.log(Number.MIN_VALUE);
+let num3=num2.toString()
+console.log(num3.length);
+
+//MATH
+console.log(Math.PI);
+console.log(Math.round(3.278));
+console.log(Math.ceil(6.8));
+console.log(Math.floor(6.7));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10+1)); //random number between 0 to 10
+let min=10
+let max=20
+console.log(Math.floor( Math.random() * (max-min+1) + min)); //random number between min to max
+
