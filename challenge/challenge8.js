6f6debabfdc1a5a715db1cf61d088ed1e364ba86
Pranita Aeer <aeerpranita@gmail.com>
2025-10-21 05:22:48 +0000

    update in repalce method

--- challenge/challenge8.js
+++ challenge/challenge8.js
@@ -9,7 +9,7 @@ console.log(str2.substring(0,4)); //cannot work in case of negative numbers
 console.log(str2.slice(4,-2)); //can work in case of negative numbers
 
 str2="http://localhost:5000/search/product%20camera"
-str2.replace("%20","-")
+str2=str2.replace("%20","-")
 console.log(str2);
 
 str2="javascript is my favourite language"
