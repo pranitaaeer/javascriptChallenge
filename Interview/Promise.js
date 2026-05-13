9e717af5beed70ef1bb445c60f7c39594d786a02
Pranita Aeer <aeerpranita@gmail.com>
2026-05-13 20:13:35 +0530

    Document Promise combinators in Promise.js
    Add comments explaining Promise combinators.

---
+++ Interview/Promise.js
@@ -0,0 +1,10 @@
+//Promise Cimbinator:
+/* 1] Promise.all() -return array of promises 
+and if any of these promise failed does not 
+execute further promise
+   2]promise.rase() - return first promise which is 
+resolve and rejected it doesn't matter
+   3]promise.allSettled() - return array of both resolve
+and rejected promises 
+   4]promise.any() - return first promise which resolve
+*/
