8df1594b6862e02fe3cc78630a00ca2eb956013b
Pranita Aeer <aeerpranita@gmail.com>
2026-05-13 20:03:22 +0530

    Add comments on Promise combinators
    Added comments explaining various Promise methods.

--- Interview/Interview1.js
+++ Interview/Interview1.js
@@ -1,4 +1,13 @@
-
+//Promise Cimbinator:
+/* 1] Promise.all() -return array of promises 
+and if any of these promise failed does not 
+execute further promise
+   2]promise.rase() - return first promise which is 
+resolve and rejected it doesn't matter
+   3]promise.allSettled() - return array of both resolve
+and rejected promises 
+   4]promise.any() - return first promise which resolve
+*/
 let arr=[1,2,4,5,1,4,5]
 
 let obj={}
