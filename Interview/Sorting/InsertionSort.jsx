ecb001ed75df5c2ad07e66e0c8fb585054aed5db
Pranita Aeer <aeerpranita@gmail.com>
2026-05-26 14:44:18 +0530

    Refactor insertionSort to use swapping method

--- Interview/Sorting/InsertionSort.jsx
+++ Interview/Sorting/InsertionSort.jsx
@@ -1,22 +1,13 @@
-function insertionSort(arr){
-
- for(let i=1;i<arr.length;i++){
-
-   let current=arr[i]
-   let j=i-1
-
-   while(j>=0 && arr[j]>current){
-
-      arr[j+1]=arr[j]
-      j--
-   }
-
-   arr[j+1]=current
- }
-
- return arr
+const insertionSort=(arr)=>{
+    for(let i=1;i<arr.length;i++){
+        let j=i-1
+        while(j>=0 && arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
+            j--
+        }
+        
+       
+    }
+    return arr
 }
-
-console.log(
- insertionSort([5,2,8,1,3])
-)
+console.log(insertionSort([9,8,7,66,5,4,11]))
