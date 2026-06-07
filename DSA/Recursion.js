03c8678d31c5a49c76b8d17e6b1abdbb312e8dd2
Pranita Aeer <aeerpranita@gmail.com>
2026-06-07 15:44:18 +0530

    Add Factorial function to Recursion.js

--- DSA/Recursion.js
+++ DSA/Recursion.js
@@ -1,7 +1,12 @@
 
-
 function recfun(arr){
     if(arr.length<=0) return 1
     return arr[arr.length-1]*recfun (arr.slice(0, arr.length-1))
 }
 console.log(recfun([1,2,3,4]))
+
+function Factorial (n){
+    if(n==1) return 1
+    return n*Factorial(n-1)
+}
+console.log(Factorial(5))
