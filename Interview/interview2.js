b63948eda40030c27c381056aff04719fd0913fc
Pranita Aeer <aeerpranita@gmail.com>
2026-06-26 17:39:22 +0530

    Implement Fibonacci function and logging
    Add a Fibonacci function to calculate Fibonacci numbers and log the first n numbers.

--- Interview/interview2.js
+++ Interview/interview2.js
@@ -1,3 +1,12 @@
+function Fibonacci(n){
+    if(n<=1) return n
+    return Fibonacci(n-1)+Fibonacci (n-2)
+}
+let n=7
+for(let i=0;i<n;i++){
+console.log(Fibonacci(i))
+    
+}
 function removeFalsy(arr){
     let newarr=[]
     for(val of arr){
