9827e6d003e26d6efa73e4cc9ec463b6af456855
Pranita Aeer <aeerpranita@gmail.com>
2026-09-27 01:04:43 +0530

    Implement vowel reversal in modify function
    Added a function to reverse vowels in a string.

--- DSA/string.js
+++ DSA/string.js
@@ -17,3 +17,29 @@
         return ans
     }
     console.log(longest(["Geek", "Geeks", "Geeksfor", "GeeksforGeek", "GeeksforGeeks"]))
+
+// reverse vowels
+ function modify(s) {
+        // code here
+        let ans=''
+        for(let val of s){
+            if(val==='a' || val==='e' || val==='i' || val==='o' || val==='u'){
+                ans+=val
+            }
+        }
+        ans=ans.split("").reverse().join('')
+        let i = 0;
+        let result = "";
+
+        for (let val of s) {
+                if ("aeiou".includes(val)) {
+                result += ans[i];
+                i++;
+                } else {
+                result += val;
+                }
+        }
+        return res
+
+}
+console.log(modify("practice"))
